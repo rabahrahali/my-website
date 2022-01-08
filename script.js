e7b5ac8a04271fe705bf3f0b09e4cdec307b5455
Rabah Rahali <75740689+rabahrahali@users.noreply.github.com>
2022-01-08 12:38:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Designer", "Collaborator", "And Problem Solver"],
+        strings: ["Designer", "Collaborator", "Problem Solver"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Designer", "Collaborator", "And Problem Solver"],
+        strings: ["Designer", "Collaborator", "Problem Solver"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
